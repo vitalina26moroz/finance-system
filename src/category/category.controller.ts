b5924668980138e2fd8ba9fddee3e82a5a056124
vita moroz <vitalina.moroz@tech-stack.io>
2024-06-03 11:14:48 +0300

    category-service

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,31 +6,45 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth-guard';
+import { Category } from './entities/category.entity';
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.create(createCategoryDto);
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
+  create(
+    @Body() createCategoryDto: CreateCategoryDto,
+    @Req() req,
+  ): Promise<Category> {
+    return this.categoryService.create(createCategoryDto, req.user.id);
   }
 
   @Get()
-  findAll() {
-    return this.categoryService.findAll();
+  @UseGuards(JwtAuthGuard)
+  findAll(@Req() req): Promise<Category[]> {
+    return this.categoryService.findAll(req.user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @UseGuards(JwtAuthGuard)
+  findOne(@Param('id') id: string): Promise<Category> {
     return this.categoryService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -39,6 +53,7 @@ export class CategoryController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.categoryService.remove(+id);
   }
--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional } from 'class-validator';
+import { User } from 'src/user/entities/user.entity';
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  category_name: string;
+  @IsNotEmpty()
+  user: User;
+  @IsOptional()
+  description: string;
+  @IsNotEmpty()
+  transaction_type: string;
+}
